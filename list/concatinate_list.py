8920859ab1bafa0a1b1610e9b3c861ed492e56ce
“Artuom <artuomkudryashov@gmail.com>
2024-02-09 17:45:44 -0500

    list refac+ extra

--- list/concatinate_list.py
+++ list/concatinate_list.py
@@ -4,8 +4,8 @@ combined_list = list1 + list2
 print(combined_list)
 
 
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-combined_list = list1 + list2
-print(combined_list)
+my_list = [1, 2, 3]
+another_list = [4, 5, 6]
+my_list.extend(another_list)
+print(my_list)
 
